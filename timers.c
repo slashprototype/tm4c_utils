9d3829bddfec6208fbf070ef848584bd6716b070
l.juarez <l.juarez@posgrado.cidesi.edu.mx>
2024-01-31 16:03:30 -0600

    A timer solution has been added, todo.. include a simple example

---
+++ timers.c
@@ -0,0 +1,63 @@
+/*
+ * timers.c
+ *
+ *  Created on: 14 dic 2023
+ *      Author: JUARELU1
+ */
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <tm4c_utils/pins.h>
+#include <tm4c_utils/timers.h>
+#include "driverlib/interrupt.h"
+#include "driverlib/timer.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "libs/heparine_pump.h"
+
+
+
+void timer1IntHandler( void ){
+    TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+}
+
+void timerDisable(timer_module_t* timer){
+    TimerIntClear(timer->hw_timer_base, timer->timer_int_mode);
+    TimerIntDisable(timer->hw_timer_base, timer->timer_int_mode);
+    TimerDisable(timer->hw_timer_base, timer->timer_name);
+}
+
+void timerEnable(timer_module_t* timer){
+    TimerIntDisable(timer->hw_timer_base, timer->timer_int_mode);
+    TimerDisable(timer->hw_timer_base, timer->timer_name);
+    /* Calculate the period value based on clock frequency*/
+    uint32_t calculated_period = (SysCtlClockGet()/timer->frequency_hz)-1;
+    TimerLoadSet(timer->hw_timer_base, timer->timer_name, calculated_period);
+
+    TimerIntEnable(timer->hw_timer_base, timer->timer_int_mode);
+    TimerEnable(timer->hw_timer_base, timer->timer_name);
+}
+
+void timerSetup( timer_module_t* timer ){
+    /* Enable timer0 peripheral if isn't enabled yet*/
+    if (!SysCtlPeripheralReady( timer->sysctl_peripheral )){
+        SysCtlPeripheralEnable( timer->sysctl_peripheral );
+        while(!SysCtlPeripheralReady( timer->sysctl_peripheral ))
+        {}
+    }
+    /* Config timer0 in periodic mode*/
+    TimerConfigure(timer->hw_timer_base, timer->timer_configure);
+    /* Prescaler is 0 for timer0, do not divide the frequency*/
+    TimerPrescaleSet(timer->hw_timer_base, timer->timer_name, timer->prescaler);
+    /* Register the interruption handler function in the dynamic vector table*/
+    TimerIntRegister(timer->hw_timer_base, timer->timer_name, timer->isrFunction);
+    /* Disable the timer interruption*/
+    TimerIntDisable(timer->hw_timer_base, timer->timer_int_mode);
+    /* Disable the timer*/
+    TimerDisable(timer->hw_timer_base, timer->timer_name);
+}
+
+
+
+
---
+++ timers.h
@@ -0,0 +1,48 @@
+/*
+ * timers.h
+ *
+ *  Created on: 14 dic 2023
+ *      Author: JUARELU1
+ */
+
+#ifndef SRC_TM4C_UTILS_TIMERS_H_
+#define SRC_TM4C_UTILS_TIMERS_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct timer_module_legacy_functions timer_module_legacy_functions_t;
+
+typedef struct{
+    uint32_t hw_timer_base;         // timer hw_memmap.h reference
+    uint32_t sysctl_peripheral;     // sysctl.h timer x reference
+    uint32_t timer_configure;       // timer.h configuration parameters
+    uint32_t timer_name;            // timer.h Timer A, B, Both, etc.
+    uint32_t timer_int_mode;        // timer.h interruption mode;
+    uint32_t frequency_hz;             // value to configure timer period
+    uint8_t prescaler;              // value to prescaler frequency
+    uint32_t counter;               // User variable, can be apply to increase every ISR handler
+    void (*isrFunction) (void);     // pointer to ISR handler function
+    timer_module_legacy_functions_t* legacy_functions;  // Default functionalities implemented
+
+}timer_module_t;
+
+struct timer_module_legacy_functions{
+    void (*timerSetup)(timer_module_t*);
+    void (*timerDisable)(timer_module_t*);
+    void (*timerEnable)(timer_module_t*);
+};
+
+
+
+/* Function to handle the timer0 ISR*/
+void timer0IntHandler( void );
+/* Setup the timer0*/
+void timerSetup( timer_module_t* timer );
+/* Enable the timer0 ISR, start the timeout ISR*/
+void timerEnable( timer_module_t* timer );
+/* Disable and clear the timer0 ISR caused by timeout of TimerA*/
+void timerDisable( timer_module_t* timer );
+
+
+#endif /* SRC_TM4C_UTILS_TIMERS_H_ */
